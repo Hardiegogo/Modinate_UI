559063aeb2bfb5d7aea20b0117041819bcad545a
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2022-02-09 04:07:07 +0530

    Update rating.js

--- CSS_files/components/rating/rating.js
+++ CSS_files/components/rating/rating.js
@@ -1,10 +1,10 @@
 const stars=document.querySelectorAll('.changeable');
 stars.forEach((star,index)=>star.addEventListener('click',()=>{
-    for(let i=0;i<=index;i++){    
+    for(let i=0;i<5;i++){    
         stars[i].classList.remove('bxs-star')
     }
     for(let i=0;i<=index;i++){    
-        stars[i].classList.toggle('bxs-star')
+        stars[i].classList.add('bxs-star')
     }
 }))
 
